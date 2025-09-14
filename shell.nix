1c5203e5d114e9de6d81d9022d6e4ed63e2dfd74
h3cth0r <hectorsucre13@gmail.com>
2025-09-14 12:21:09 -0600

    security dataset analysis

---
+++ shell.nix
@@ -0,0 +1,36 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+pkgs.mkShell {
+  buildInputs = [
+    pkgs.python311
+    pkgs.gcc
+    pkgs.zeromq
+    # Add additional dependencies that might be needed
+    pkgs.pkg-config
+    pkgs.libffi
+    pkgs.openssl
+  ];
+  
+  # Fixed LD_LIBRARY_PATH - this is the key fix
+  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+    pkgs.stdenv.cc.cc.lib  # This provides libstdc++.so.6
+    pkgs.zeromq
+    pkgs.libffi
+    pkgs.openssl
+  ];
+  
+  shellHook = ''
+    unset SOURCE_DATE_EPOCH
+    if [ ! -d ".venv" ]; then
+      echo "Creating Python virtual environment..."
+      python -m venv .venv
+      source .venv/bin/activate
+      echo "Installing packages from requirements.txt..."
+      pip install -r requirements.txt
+    else
+      source .venv/bin/activate
+    fi
+    echo "Nix shell is ready. You can now run 'jupyter notebook'."
+  '';
+}
